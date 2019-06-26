9811b958fbd1db66b6975fa9b261a7370f43c311
Priyanka Vagadre <priyanka.vagadre@EXXATSYSTEMS.COM>
2019-06-26 17:53:23 +0530

    custom validations with dynamic validations

--- my-app/src/app/create-employee/create-employee.component.ts
+++ my-app/src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup , FormControl, Validator} from '@angular/forms'
+import {FormGroup , FormControl, Validator, FormBuilder, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-create-employee',
@@ -8,20 +8,83 @@ import {FormGroup , FormControl, Validator} from '@angular/forms'
 })
 export class CreateEmployeeComponent implements OnInit {
   employeeForm : FormGroup;
+  validationMessages ={
+    'fullName': {
+      'required' : 'Full Name is Required',
+      'minlength' : 'Min length should be 5',
+      'maxlength' : 'Max length should be 10'
+    },
+    'email':{
+      'required' : 'email is Required',
+    },
+    'phone':{
+      'required' : 'phone is Required',
+    }
+  };
+  formErrors = {
+    'fullName': '',
+    'email': '',
+    'phone' : ''
+  }
 
-  constructor() { }
+  constructor(public formBuild : FormBuilder) { }
 
   ngOnInit() {
-    this.employeeForm = new FormGroup({
-         fullname : new FormControl(''),
-         emailId :  new FormControl(''),
-         skills : new FormGroup({
-            skillsName : new FormControl(''),
-            exp : new FormControl(''),
-            proficiency : new FormControl(''),
-         })
-    });
+    // this.employeeForm = new FormGroup({
+    //      fullname : new FormControl(''),
+    //      emailId :  new FormControl(''),
+    //      skills : new FormGroup({
+    //         skillsName : new FormControl(''),
+    //         exp : new FormControl(''),
+    //         proficiency : new FormControl(''),
+    //      })
+    // });
+
+    this.employeeForm = this.formBuild.group({
+      fullname : ['' , [Validators.required , Validators.minLength(5) , Validators.maxLength(10)]],
+      emailId :  ['', Validators.required],
+      phone : [''],
+      contactPreference : [''],
+           skills : this.formBuild.group({
+              skillsName : [''],
+              exp :[''],
+              proficiency : [''],
+           })
+    })
+    this.employeeForm.get('fullname').valueChanges.subscribe(
+      value =>{
+        console.log(value);
+      }
+    )
+    this.employeeForm.get('emailId').valueChanges.subscribe(
+      value =>{
+       console.log(value);
+
+      }
+    )
+
+    this.employeeForm.valueChanges.subscribe(
+      (values) =>{
+        console.log(values);
+        console.log(JSON.stringify(values));
+
+      }
+    )
+
+
   }
+  onCheckedValue(selectedValue : string){
+    const phoneControl  = this.employeeForm.get('phone');
+    if(selectedValue === 'phone'){
+      phoneControl.setValidators(Validators.required)
+    }
+    else{
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+
+  }
+
   onSubmit(){
     console.log(this.employeeForm.value);
     // console.log(this.employeeForm.controls.emailId.value);
@@ -37,6 +100,8 @@ export class CreateEmployeeComponent implements OnInit {
     this.employeeForm.setValue({
       fullname : 'Priyanka',
       emailId : 'priyanka@gmail.com',
+      phone : '567567567567',
+      contactPreference : 'phone',
          skills : {
             skillsName : 'Angular',
             exp : '4 Months',
