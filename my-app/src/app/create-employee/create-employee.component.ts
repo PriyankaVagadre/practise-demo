ecfaba531314147f7697afd511e43c9e5e5b62af
Priyanka Vagadre <priyanka.vagadre@EXXATSYSTEMS.COM>
2019-06-26 13:04:14 +0530

    load data from form

--- my-app/src/app/create-employee/create-employee.component.ts
+++ my-app/src/app/create-employee/create-employee.component.ts
@@ -14,12 +14,39 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit() {
     this.employeeForm = new FormGroup({
          fullname : new FormControl(''),
-         emailId :  new FormControl('')
+         emailId :  new FormControl(''),
+         skills : new FormGroup({
+            skillsName : new FormControl(''),
+            exp : new FormControl(''),
+            proficiency : new FormControl(''),
+         })
     });
   }
   onSubmit(){
     console.log(this.employeeForm.value);
+    // console.log(this.employeeForm.controls.emailId.value);
+    // console.log(this.employeeForm.controls.emailId.invalid);
+    // console.log(this.employeeForm.controls.emailId.dirty);
+    // console.log(this.employeeForm.dirty)
 
+  }
+  addNewSkills(){
+
+  }
+  loadData(){
+    this.employeeForm.setValue({
+      fullname : 'Priyanka',
+      emailId : 'priyanka@gmail.com',
+         skills : {
+            skillsName : 'Angular',
+            exp : '4 Months',
+            proficiency : 'beginner',
+         }
+    })
+    // this.employeeForm.patchValue({
+    //   fullname : 'Priyanka',
+    //   emailId : 'priyanka@gmail.com',
+    // })
   }
 
 }
